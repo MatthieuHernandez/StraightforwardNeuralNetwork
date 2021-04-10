b2290dfbe7ee7cd1d489867f70ff7a2736f6c095
Matthieu HERNANDEZ <matthieu.hernandez1994@gmail.com>
2021-04-10 22:57:47 +0200

    Improve 1D filter layers

--- src/neural_network/layer/Convolution1D.cpp
+++ src/neural_network/layer/Convolution1D.cpp
@@ -39,20 +39,18 @@ int Convolution1D::isValid() const
 }
 
 inline
-vector<float> Convolution1D::createInputsForNeuron(int neuronNumber, const vector<float>& inputs)
+vector<float> Convolution1D::createInputsForNeuron(const int neuronIndex, const vector<float>& inputs)
 {
-    neuronNumber = neuronNumber % this->getNumberOfNeurons() / this->numberOfFilters;
-    const int beginIndex = neuronNumber * this->shapeOfInput[1];
-    const int endIndex = (neuronNumber + this->sizeOfFilterMatrix) * this->shapeOfInput[1];
+    const int beginIndex = neuronIndex * this->shapeOfInput[1];
+    const int endIndex = (neuronIndex + this->sizeOfFilterMatrix) * this->shapeOfInput[1];
     return vector<float>(inputs.begin() + beginIndex, inputs.begin() + endIndex);
 }
 
 inline
-void Convolution1D::insertBackOutputForNeuron(int neuronNumber, const std::vector<float>& error,
+void Convolution1D::insertBackOutputForNeuron(const int neuronIndex, const std::vector<float>& error,
                                               std::vector<float>& errors)
 {
-    neuronNumber = neuronNumber % this->getNumberOfNeurons() / this->numberOfFilters;
-    const int beginIndex = neuronNumber * this->shapeOfInput[1];
+    const int beginIndex = neuronIndex * this->shapeOfInput[1];
     for (int e = 0; e < (int)error.size(); ++e)
     {
         const int i = beginIndex + e;

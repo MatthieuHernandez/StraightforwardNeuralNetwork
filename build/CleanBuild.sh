f76b6780257f90159612a8a58ffbc3c29f89c020
Matthieu HERNANDEZ <matthieu.hernandez1994@gmail.com>
2022-10-22 18:10:12 +0200

    Update CleanBuild.sh

--- build/CleanBuild.sh
+++ build/CleanBuild.sh
@@ -3,6 +3,12 @@ rm -rf ./CMakeFiles
 rm -rf ./lib
 rm -rf ./src
 rm -rf ./tests
-rm ./cmake_install.cmake
-rm ./CMakeCache.txt
-rm ./Makefile
+rm -f ./cmake_install.cmake
+rm -f ./CMakeCache.txt
+
+# Remove Visual Studio build files
+rm -f ./*.vcxproj*
+rm -f ./*.sln*
+rm -rf ./.vs
+rm -rf ./x86
+rm -rf ./x64

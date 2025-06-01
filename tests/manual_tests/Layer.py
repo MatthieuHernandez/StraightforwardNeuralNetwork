1dd981701505180cf32db4b6bba46b2ac46e244d
Johansmm <jmejia_26@hotmail.com>
2025-06-01 23:47:39 +0200

    test(layer_info): print new weights/outputs

--- tests/manual_tests/Layer.py
+++ tests/manual_tests/Layer.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 print("TensorFlow version:", tf.__version__)
 
 
-def __get_layer_output(layer, new_weights, input, expected):
+def __get_layer_output(layer, new_weights, input, expected, lr=0.001):
     # Build the layer
     layer.build(input.shape)
 
@@ -24,12 +24,16 @@ def __get_layer_output(layer, new_weights, input, expected):
 
     # Compute gradients
     grads = tape.gradient(loss, [input, *layer.trainable_variables])
+
+    # Update weights
+    for v, g in zip(layer.trainable_variables, grads[1:]):
+        v.assign_sub(lr * g)
     return output, grads
 
 
-def layer_info(forward_layer, weights, input, expected):
+def layer_info(forward_layer, weights, input, expected, lr=0.001):
     # Do a forward propagation
-    forward_output, grads = __get_layer_output(forward_layer, weights, input, expected)
+    forward_output, grads = __get_layer_output(forward_layer, weights, input, expected, lr=lr)
 
     # Print the results
     print("============================= input =============================")
@@ -41,3 +45,10 @@ def layer_info(forward_layer, weights, input, expected):
     print("============================= error =============================")
     backward_output = grads[0]
     print(backward_output)
+    print("========================== new_weights ==========================")
+    print(forward_layer.weights[0])
+    if getattr(forward_layer, "use_bias", False):
+        print("============================ new_bias ===========================")
+        print(forward_layer.weights[1])
+    print("========================== new_outputs ==========================")
+    print(forward_layer(input))
